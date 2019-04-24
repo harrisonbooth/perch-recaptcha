25da645fe6d667c885aef103ac2f1e01954c9d5a
Harrison Booth <harrison.booth@intellicore.co.uk>
2019-04-24 10:19:47 +0100

    Add skeleton of app

---
+++ runtime.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+     * 
+     * Receives a form submission and validate using Recaptcha, re-dispatching the form to Perch forms if valid.
+     * @param $form
+     */
+    function hbooth_recaptcha_form_handler($form)
+    {
+        $formData = $form->data;
+        $response = null;
+
+        if(isset($formData['g-recaptcha-response'])) {
+            $response = $formData['g-recaptcha-response'];
+        }
+
+        $recaptcha = new \Recaptcha\Recaptcha('SECRET_API_KEY');
+        $recaptcha->validate($response, $_SERVER['REMOTE_ADDR']);
+        if($recaptcha->isValid()) {
+            $form->redispatch('perch_forms');
+        } else {
+            $form->throw_error('ERROR', 'Failed to pass Recaptcha.');
+        }
+    }
+
+    
